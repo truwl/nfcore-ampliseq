3166a592b053526478f7ccd1c2da8b16beaf54b1
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:28:38 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-ampliseq:2.2.0_0.1.0 -t truwl/nfcore-ampliseq:latest .
+docker push truwl/nfcore-ampliseq:2.2.0_0.1.0
+docker push truwl/nfcore-ampliseq:latest
+        
